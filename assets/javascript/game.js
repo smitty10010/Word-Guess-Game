fa412c89c34109fea06d616bc83bafb3f1495c6c
Robert <35673753+smitty10010@users.noreply.github.com>
2018-06-05 14:14:50 -0600

    added initial pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//Need an event listener for keypress to start game
+
+//Once game is started, the computer chooses at random a word from the array of words
+
+//Blank spaces appear to the user where the chosen word would be, example "_ _ _ _ _"
+
+//The remaining guesses begins at 13
+
+//The letter the player guesses incorrectly appears in the already guest box
+
+//The letter the player guesses correctly appears in the blank spot of the current word box example "_ h _"
+
+//If the player runs out of guesses then game resets and alerts player that they have lost
+
+//If the player finishes the word before running out of guesses then the game resets with a new word and the win counter goes up
