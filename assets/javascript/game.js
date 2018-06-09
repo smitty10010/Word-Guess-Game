42a3213def1cf55b8e9f15ac287ce8d4ae3ef345
Robert <35673753+smitty10010@users.noreply.github.com>
2018-06-09 17:58:32 -0600

    created if and forloop statments.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,11 @@
 //Need an event listener for keypress to start game //Create an array for the computer to choose from 
-var tggArray = ["Bangers", "Veto", "Shimoda", "Entrepreneur", "Ro"];
+// var tggArray = ["Bangers", "Veto", "Shimoda", "Entrepreneur", "Ro"];
+var tggArray = ["Entrepreneur"];
 var userKeys = [];
 var guessCounter = 13;
+var spacesForLetters = [];
+var abc = "abcdefghijklmnopqrstuvwxyz";
+var abcArray = abc.split("", abc.length);
 //Once game is started, the computer chooses at random a word from the array of words
 
 function computerChoice() {
@@ -9,15 +13,28 @@ function computerChoice() {
     return tggArray[randomInt].toLowerCase();
 }
 var compChoice = computerChoice().toString();
-console.log(compChoice);
+var compChoiceArray = compChoice.split("", compChoice.length);
+var BlankArray = [];
+console.log(compChoiceArray);
+
 //Blank spaces appear to the user where the chosen word would be, example "_ _ _ _ _"
 
-function createBlankSpots(string) {
-    for (var i = 0; i <= string.length; i++) {
-        document.getElementById("current-word").textContent = " _ ".repeat(i);
-    }
+function createBlankSpots(array) {
+    compChoiceBlankArray = BlankArray.concat(array);
+    compChoiceBlankArray.fill("_")
+    document.getElementById("current-word").textContent = compChoiceBlankArray.join(" ");
 }
-createBlankSpots(compChoice);
+createBlankSpots(compChoiceArray);
+
+function guessedLetters(array) {
+    document.getElementById("current-word").textContent = array.join(" ");
+}
+
+//split compChoice into array
+
+// var compChoiceArray = compChoice.split("", compChoice.length);
+// console.log(compChoiceArray);
+
 //The remaining guesses begins at 13 
 
 function guessCounterD() {
@@ -29,15 +46,29 @@ guessCounterD();
 //The letter the player guesses incorrectly appears in the already guessed box
 
 var userGuess = document.onkeyup = function(event) {
-    var keyPress = event.key;
-    userKeys.push(" " + keyPress);
-    document.getElementById("guessed-letters").textContent = userKeys;
+    var keyPress = event.key.toLowerCase();
 
-    if (compChoice.includes(keyPress)) {
+    if (compChoiceArray.includes(keyPress)) {
+        for (i = 0; i < compChoiceArray.length; i++) {
+            var indexCompChoice = compChoiceArray.indexOf(keyPress, i);
+            compChoiceBlankArray[indexCompChoice] = keyPress;
+            guessedLetters(compChoiceBlankArray);
+        }
+    } else if (abcArray.includes(keyPress) !== true) {
+        event.stopPropagation();
+
+    } else if (userKeys.includes(keyPress)) {
+        event.stopPropagation();
+    } else {
         document.getElementById("guess-counter").textContent = " " + guessCounter--;
+        userKeys.push(keyPress);
+        document.getElementById("guessed-letters").textContent = " " + userKeys;
+
     }
-};
+}
 
 //The letter the player guesses correctly appears in the blank spot of the current word box example "_ h _" 
+
 //If the player runs out of guesses then game resets and alerts player that they have lost 
-//If the player finishes the word before running out of guesses then the game resets with a new word and the win counter goes up
+
+// If the player finishes the word before running out of guesses then the game resets with a new word and the win counter goes up
