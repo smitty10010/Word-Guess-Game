edee794bde8877876957ca6520182f0ef9768947
Robert <35673753+smitty10010@users.noreply.github.com>
2018-06-10 23:48:42 -0600

    updated tags

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,41 @@
-//Need an event listener for keypress to start game //Create an array for the computer to choose from 
-// var tggArray = ["Bangers", "Veto", "Shimoda", "Entrepreneur", "Ro"];
-var tggArray = ["Entrepreneur"];
+//establish vars up front
+
+var tggArray = ["Previa", "Rikerlean", "Shimoda", "Entrepreneur", "Spacebutthole", "Wesley", "Mclaughlingroup", "Yeager"];
+var wins = 1;
+var losses = 1;
 var userKeys = [];
 var guessCounter = 13;
 var spacesForLetters = [];
 var abc = "abcdefghijklmnopqrstuvwxyz";
 var abcArray = abc.split("", abc.length);
+var compChoice = computerChoice().toString();
+var compChoiceArray = compChoice.split("", compChoice.length);
+var audio;
+var compChoiceBlankArray;
+var BlankArray = [];
+
+//Game restart peramitors
+
+function restartGame() {
+    computerChoice();
+    compChoice = computerChoice().toString();
+    compChoiceArray = compChoice.split("", compChoice.length);
+    createBlankSpots(compChoiceArray);
+    selectImage(compChoice);
+    selectAudio(compChoice);
+    guessCounter = 13;
+    setGuessCounter();
+    userKeys = [];
+    document.getElementById("guessed-letters").textContent = " " + userKeys;
+}
+console.log(compChoice);
+
 //Once game is started, the computer chooses at random a word from the array of words
 
 function computerChoice() {
     var randomInt = Math.floor(Math.random() * Math.floor(tggArray.length));
     return tggArray[randomInt].toLowerCase();
 }
-var compChoice = computerChoice().toString();
-var compChoiceArray = compChoice.split("", compChoice.length);
-var BlankArray = [];
-console.log(compChoiceArray);
 
 //Blank spaces appear to the user where the chosen word would be, example "_ _ _ _ _"
 
@@ -30,10 +50,30 @@ function guessedLetters(array) {
     document.getElementById("current-word").textContent = array.join(" ");
 }
 
+// selects image for the computer's word
+
+function selectImage(word) {
+    document.getElementById("image").setAttribute("src", "assets/images/" + word + ".jpg");
+}
+
+selectImage(compChoice);
+
+//Selects audio file
+function selectAudio(word) {
+    newElement = document.createElement("AUDIO");
+    newElement.setAttribute("src", "assets/audio/" + word + ".mp3");
+    newElement.setAttribute("id", "audioFile");
+    document.getElementById("audio").appendChild(newElement);
+    audio = document.getElementById("audioFile");
+}
+
+selectAudio(compChoice);
+
 //The remaining guesses begins at 13 
 
-
-document.getElementById("guess-counter").textContent = " " + guessCounter;
+function setGuessCounter() {
+    document.getElementById("guess-counter").textContent = " " + guessCounter;
+}
 
 //The letter the player guesses incorrectly appears in the already guessed box
 
@@ -48,22 +88,37 @@ var userGuess = document.onkeyup = function(event) {
             compChoiceBlankArray[indexCompChoice] = keyPress;
             guessedLetters(compChoiceBlankArray);
         }
-        //checks to make sure the key pressed is a-z
+        //checks to see if the player has won. If they have then the game resets and wins are recorded
+
+        if (compChoiceBlankArray.join("").toString() == compChoiceArray.join("").toString()) {
+            audio.play();
+            alert("You guessed " + compChoice + " correctly! You have won!");
+            document.getElementById("wins").textContent = " " + wins++;
+            restartGame();
+        }
+        //checks to see if the key is not a-z
+
     } else if (abcArray.includes(keyPress) !== true) {
         event.stopPropagation();
+
         //checks to see if the user already used that key
+
     } else if (userKeys.includes(keyPress)) {
         event.stopPropagation();
+
+        //checkes to see if player has ran out of guesses. If so then game resets and loss counter goes up
+
+    } else if (guessCounter == 0) {
+        alert("Sorry, the word was " + compChoice + ". You have lost!");
+        document.getElementById("losses").textContent = " " + losses++;
+        restartGame();
+
         //if user guess wrong then logs key in already guessed array and deprecates counter
+
     } else {
         document.getElementById("guess-counter").textContent = " " + guessCounter--;
         userKeys.push(keyPress);
         document.getElementById("guessed-letters").textContent = " " + userKeys;
-
     }
+
 }
-
-
-//If the player runs out of guesses then game resets and alerts player that they have lost 
-
-// If the player finishes the word before running out of guesses then the game resets with a new word and the win counter goes up
